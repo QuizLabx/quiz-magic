19739b1e3e4045f1ec825556419558f6a0e054a8
QuizLabx <quizmagicsupport@gmail.com>
2026-06-29 00:47:30 +0100

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,22 @@
+/**
+ * QuizMagic Configuration File
+ * استخدم هذا الملف لتحديث روابطك وإعدادات الموقع بسهولة
+ */
+
+const siteConfig = {
+    // روابط وسائل التواصل الاجتماعي - ضع روابطك هنا
+    socialLinks: {
+        facebook: "https://facebook.com/yourpage",
+        twitter: "https://twitter.com/yourprofile",
+        instagram: "https://instagram.com/yourprofile",
+        youtube: "https://youtube.com/yourchannel",
+        linkedin: "https://linkedin.com/in/yourprofile"
+    },
+    
+    // إعدادات عامة
+    settings: {
+        showWelcomeScreen: true, // إظهار شاشة "كيف يعمل الاختبار" قبل البدء
+        analysisDelay: 3500,     // مدة ظهور شاشة "جاري التحليل" (بالملي ثانية)
+        enableShare: true        // تفعيل ميزة المشاركة
+    }
+};
